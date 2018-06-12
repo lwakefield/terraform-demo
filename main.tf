557690cd88cff498ac8c5397aeabbb87ca53899a
Lawrence Wakefield <lawrence@iamlawrence.me>
2018-06-12 14:32:39 -0400

    Add loadbalancer

--- main.tf
+++ main.tf
@@ -24,3 +24,22 @@ resource "digitalocean_droplet" "server" {
     ]
   }
 }
+
+resource "digitalocean_loadbalancer" "public" {
+  name = "public-loadbalancer"
+  region = "nyc1"
+
+  droplet_ids = ["${digitalocean_droplet.server.*.id}"]
+
+  forwarding_rule {
+    entry_port = 80
+    entry_protocol = "http"
+    target_port = 80
+    target_protocol = "http"
+  }
+
+  healthcheck {
+    port = 22
+    protocol = "tcp"
+  }
+}
