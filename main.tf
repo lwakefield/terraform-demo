8ed44f9706fc975aba6aa219ae8cec6935e2250c
Lawrence Wakefield <lawrence@iamlawrence.me>
2018-06-12 14:26:43 -0400

    Add server

--- main.tf
+++ main.tf
@@ -8,3 +8,18 @@ resource "digitalocean_ssh_key" "default" {
   name = "terraform"
   public_key = "${file("./id_rsa.pub")}"
 }
+
+resource "digitalocean_droplet" "server" {
+  name = "server"
+  region = "nyc1"
+  size = "1gb"
+  image = "docker-16-04"
+  ssh_keys = [ "${digitalocean_ssh_key.default.id}" ]
+  private_networking = true
+
+  provisioner "remote-exec" {
+    inline = [
+      "docker run -d -p 80:8000 jwilder/whoami"
+    ]
+  }
+}
